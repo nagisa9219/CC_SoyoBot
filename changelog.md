e85701e5f4b831dec9b44c8751fc9d9133a3a9ec
nagisadesu87 <nagisadesu87@gmail.com>
2024-10-23 00:32:25 +0800

    Fix: Fix voice chat logged incorrectly. Resolves: #1

--- changelog.md
+++ changelog.md
@@ -1,7 +1,14 @@
 # Changelog
 
+## [0.0.1-Beta.2] - 24-10-23
+
+### Fixed
+
+- Voice chat logged incorrectly as user joined/ left when the user's voice state updated. ([#1](https://github.com/nagisa9219/CC_SoyoBot/issues/1))
+
 ## [0.0.1-Beta] - 24-10-20
 
 _Initial release._
 
 [0.0.1-Beta]: https://github.com/nagisa9219/CC_SoyoBot/releases/tag/v0.0.1-Beta
+[0.0.1-Beta.2]: https://github.com/nagisa9219/CC_SoyoBot/releases/tag/v0.0.1-Beta.2
--- src/cogs/vc_logging.py
+++ src/cogs/vc_logging.py
@@ -12,27 +12,30 @@ class VCLogging(commands.Cog):
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState, after:discord.VoiceState):
-        if before.channel is not None:
-            embedmsg = discord.Embed(
-                description=f"<@{member.id}> has left the voice channel.",
-                timestamp=datetime.datetime.now(),
-                footer=discord.EmbedFooter(text=f"#{before.channel}"),
-                color=discord.Colour.red()
-                )
-            embedmsg.set_author(name=member.name, icon_url=member.avatar)
-            await before.channel.send(embed=embedmsg)
-            logger.info(f"User {member.name} ({member.id}) has left the voice channel {before.channel} ({before.channel.id}).")
+        if before.channel == after.channel:
+            pass
+        else:
+            if before.channel is not None:
+                embedmsg = discord.Embed(
+                    description=f"<@{member.id}> has left the voice channel.",
+                    timestamp=datetime.datetime.now(),
+                    footer=discord.EmbedFooter(text=f"#{before.channel}"),
+                    color=discord.Colour.red()
+                    )
+                embedmsg.set_author(name=member.name, icon_url=member.avatar)
+                await before.channel.send(embed=embedmsg)
+                logger.info(f"User {member.name} ({member.id}) has left the voice channel {before.channel} ({before.channel.id}).")
 
-        if after.channel is not None:
-            embedmsg = discord.Embed(
-                description=f"<@{member.id}> has joined the voice channel.",
-                timestamp=datetime.datetime.now(),
-                footer=discord.EmbedFooter(text=f"#{after.channel}"),
-                color=discord.Colour.green()
-                )
-            embedmsg.set_author(name=member.name, icon_url=member.avatar)
-            await after.channel.send(embed=embedmsg)
-            logger.info(f"User {member.name} ({member.id}) has joined the voice channel {after.channel} ({after.channel.id}).")
+            if after.channel is not None:
+                embedmsg = discord.Embed(
+                    description=f"<@{member.id}> has joined the voice channel.",
+                    timestamp=datetime.datetime.now(),
+                    footer=discord.EmbedFooter(text=f"#{after.channel}"),
+                    color=discord.Colour.green()
+                    )
+                embedmsg.set_author(name=member.name, icon_url=member.avatar)
+                await after.channel.send(embed=embedmsg)
+                logger.info(f"User {member.name} ({member.id}) has joined the voice channel {after.channel} ({after.channel.id}).")
 
 def setup(bot): 
     bot.add_cog(VCLogging(bot)) 

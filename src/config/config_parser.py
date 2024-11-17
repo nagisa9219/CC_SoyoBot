dc5081d0896ffd6f0e7ea3bb57718f8bfcca1148
nagisadesu87 <nagisadesu87@gmail.com>
2024-11-17 21:10:00 +0800

    Feat: Added auto generate new config file

--- src/config/config_parser.py
+++ src/config/config_parser.py
@@ -1,6 +1,34 @@
-import os, json
+import os
+import json
+from src.logger import logger
 
-def config_load() -> dict:
-    with open(f"{os.getcwd()}/src/config/config.json") as rconfig:
+config_path = os.getcwd()
+config_default = {
+    "credentials": {
+        "token": ""
+    },
+    "permissions": {
+        "owner_id": "auto"
+    },
+    "cogs": {
+        "enabled_cogs": [
+            "vc_logging",
+            "random_pick"
+        ]
+    }
+}
+
+def config_create(src_path: str) -> int:
+    if os.path.isfile(f"{src_path}/config/config.json"):
+        logger.error("Action aborted because config.json exists.")
+        return 0
+    else:
+        with open(f"{src_path}/config/config.json", "w", encoding="utf-8") as config:
+            json.dump(config_default, config, ensure_ascii=False)
+        logger.info("Created config.json.")
+        return 1
+
+def config_load(src_path: str) -> dict:
+    with open(f"{src_path}/config/config.json") as rconfig:
         config = json.load(rconfig)
     return config
--- src/main.py
+++ src/main.py
@@ -3,23 +3,28 @@ from discord.ext import commands
 import os
 import dotenv
 import json
-from logger import logger
+from src.logger import logger
+from config.config_parser import config_load, config_create
 
 dotenv.load_dotenv()
 intents = discord.Intents.all()
-intents.message_content = True
-intents.members = True
 bot = commands.Bot(intents=intents)
 
-@bot.event
-async def on_ready():
-    logger.info(f"{bot.user} is ready and online!")
+src_path = os.getcwd()
 
-with open(f"{os.getcwd()}/src/config/config.json") as rconfig:
-    config = json.load(rconfig)
+if not os.path.isfile(f"{src_path}/config/config.json"):
+    config_create(src_path)
+
+config = config_load(src_path)
+
 enabled_cogs = config["cogs"]["enabled_cogs"]
 for cog in enabled_cogs:
     bot.load_extension(f"cogs.{cog}")
 logger.info(f"Loaded cogs: {",".join(enabled_cogs)}" )
+
+
+@bot.event
+async def on_ready():
+    logger.info(f"{bot.user} is ready and online!")
 
 bot.run(os.getenv('TOKEN'))
